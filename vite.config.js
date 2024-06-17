6ba3eed05ef371b2525ea087bac17b983e960e52
Rohan Mukherjee <roerohan@gmail.com>
2024-06-17 15:37:29 +0530

    fix: update build config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -7,7 +8,16 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src', 'index.html'),
+        about_us: resolve(__dirname, 'src', 'about_us.html'),
+        arrah_gardens: resolve(__dirname, 'src', 'arrah_gardens.html'),
+        avkash_residences: resolve(__dirname, 'src', 'avkash_residences.html'),
+      }
+    },
     outDir: "../dist",
+    emptyOutDir: true,
   },
   base: process.env.CI ? process.env.REPOSITORY_NAME : '',
 });
